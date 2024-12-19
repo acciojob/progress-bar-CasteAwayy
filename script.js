852a93bc2ca6f6bc757643b8e24197adfc406227
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-19 20:31:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+// "use strict";
+
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const circles = document.querySelectorAll(".circle");
+const span = document.querySelector("span");
+
+let counter = 0;
+next.addEventListener("click", function () {
+  counter++;
+  if (counter > 0) prev.removeAttribute("disabled");
+  circles[counter].classList.add("active");
+  if (counter == 4) this.setAttribute("disabled", true);
+
+  span.style.width = `${25 * counter}%`;
+});
+
+prev.addEventListener("click", function () {
+  if (next.getAttribute("disabled") === "true") {
+    next.removeAttribute("disabled");
+  }
+  circles[counter].classList.remove("active");
+  counter--;
+  span.style.width = `${25 * counter}%`;
+  if (counter == 0) {
+    this.setAttribute("disabled", true);
+  }
+});
